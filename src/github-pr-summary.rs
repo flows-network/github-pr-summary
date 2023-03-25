ed92dd7a0daf20d4c8f8a597d9e5d8545e3f67a6
Ji Chen <jaykchen@gmail.com>
2023-03-25 15:27:11 -0400

    ask gpt to visit diff url directly

--- src/github-pr-summary.rs
+++ src/github-pr-summary.rs
@@ -55,119 +55,36 @@ async fn handler(owner: &str, repo: &str, openai_key_name: &str, payload: EventP
         "https://patch-diff.githubusercontent.com/raw/{owner}/{repo}/pull/{pull_number}.diff"
     );
 
-    let uri = Uri::try_from(diff_url.as_str()).unwrap();
+    let prompt = format!("@chatgptbot, read {diff_url}, which is the code diff document for a pull request on GitHub, please summarize it into key points");
+    // let prompt_start = format!("Contributor {contributor} filed the pull request titled {title}, proposing changes as shown in plain text diff record at the end of this message");
 
-    let mut writer = Vec::new();
-
-    let _ = Request::new(&uri)
-        .method(Method::GET)
-        .header("Accept", "application/vnd.github+json")
-        .header("User-Agent", "Github Connector of Second State Reactor")
-        .send(&mut writer)
-        .map_err(|_e| {})
-        .unwrap();
-
-    let diff_as_text = String::from_utf8_lossy(&writer);
-
-    let prompt_start = format!("Contributor {contributor} filed the pull request titled {title}, proposing changes as shown in plain text diff record at the end of this message");
-
-    if diff_as_text.lines().count() < 210 {
-        let prompt = format!("{prompt_start}, please summarize into key points by order of importance: {diff_as_text}");
-
-        let co = ChatOptions {
-            model: ChatModel::GPT35Turbo,
-            restart: true,
-            restarted_sentence: Some(&prompt),
-        };
-
-        if let Some(r) = chat_completion(
-            openai_key_name,
-            &format!("PR#{}", pull_number),
-            &prompt,
-            &co,
-        ) {
-            send_message_to_channel("ik8", "general", r.choice);
-        }
-    } else {
-        let chunked = chunk_input(diff_as_text.to_string());
-        let mut prompt_arr = serial_prompts(chunked, prompt_start);
-        let count = prompt_arr.len();
-        let mut count_down = count.clone();
-        while let Some(prompt) = prompt_arr.pop() {
-            let co = ChatOptions {
-                model: ChatModel::GPT35Turbo,
-                restart: true,
-                restarted_sentence: Some(&prompt),
-            };
-
-            if let Some(r) = chat_completion(
-                openai_key_name,
-                &format!("PR#{}", pull_number),
-                &prompt,
-                &co,
-            ) {
-                if count_down == count {
-                    send_message_to_channel(
-                        "ik8",
-                        "general",
-                        "please wait..., it could take minutes to summarize a large pull request"
-                            .to_string(),
-                    );
-                } else if count_down == 1 {
-                    send_message_to_channel("ik8", "general", r.choice);
-                }
-                count_down -= 1;
-            }
-        }
-    }
-}
-
-pub fn chunk_input(inp: String) -> Vec<String> {
-    let mut res = Vec::<String>::new();
-
-    let mut chunks = inp.split_inclusive("diff --git").collect::<Vec<&str>>();
-    chunks.remove(0); // Remove the first empty element
-
-    for chunk in chunks {
-        let chunk = chunk.lines().take(210).collect::<Vec<&str>>().join("\n");
-        res.push(chunk);
-    }
-    res
-}
-
-pub fn serial_prompts(inp_vec: Vec<String>, prompt_start: String) -> Vec<String> {
-    use std::collections::VecDeque;
-    let mut prompt_vec = Vec::new();
-    let mut deq = inp_vec.into_iter().collect::<VecDeque<String>>();
-
-    let start_text = deq.pop_front().unwrap();
-
-    let last_text = deq.pop_back().unwrap();
-
-    let start_prompt = format!("I have a long GitHub pull request diff document in plain text and will process it in smaller chunks. Process chunk 1 and provide a short summary: {start_text}");
-    let last_prompt = format!("This is part of the code diff file of a GitHub pull request, please process this chunk and provide a short summary: {last_text}");
-    let end_prompt = format!("Finally, these chunks just proccessed are the core data of a pull request on GitHub, {prompt_start}, please summarize the pull request into key points by order of importance");
-
-    match deq.len() {
-        0 => {
-            let middle_prompt = last_prompt.clone();
-
-            return vec![end_prompt, middle_prompt, start_prompt];
-        }
-        1.. => {
-            while let Some(diff_as_text) = deq.pop_back() {
-                let mid_prompt = format!(
-                    "Please process this chunk and provide a short summary: {diff_as_text}"
-                );
-
-                prompt_vec.push(mid_prompt);
-            }
-            prompt_vec.insert(0, end_prompt);
-            prompt_vec.push(start_prompt);
-            return prompt_vec;
-        }
-        _ => {
-            unreachable!()
-        }
+    // let chunked = chunk_input(diff_as_text.to_string());
+    // let mut prompt_arr = serial_prompts(chunked, prompt_start);
+    // let count = prompt_arr.len();
+    // let mut count_down = count.clone();
+    // while let Some(prompt) = prompt_arr.pop() {
+    let co = ChatOptions {
+        model: ChatModel::GPT35Turbo,
+        restart: true,
+        restarted_sentence: Some(&prompt),
     };
+
+    if let Some(r) = chat_completion(
+        openai_key_name,
+        &format!("PR#{}", pull_number),
+        &prompt,
+        &co,
+    ) {
+        // if count_down == count {
+        //     send_message_to_channel(
+        //         "ik8",
+        //         "general",
+        //         "please wait..., it could take minutes to summarize a large pull request"
+        //             .to_string(),
+        //     );
+        // } else if count_down == 1 {
+        send_message_to_channel("ik8", "general", r.choice);
+        // }
+        // count_down -= 1;
+    }
 }
