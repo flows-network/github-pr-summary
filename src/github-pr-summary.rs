1a19af283f173396a4742c210f56a338afa5f9f4
Michael Yuan <michael@secondstate.io>
2023-04-12 08:26:31 +0000

    Use default API key
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/github-pr-summary.rs
+++ src/github-pr-summary.rs
@@ -9,7 +9,7 @@ use http_req::{
     request::{Method, Request},
     uri::Uri,
 };
-use openai_flows::{chat_completion, ChatModel, ChatOptions};
+use openai_flows::{chat_completion_default_key, ChatModel, ChatOptions};
 use std::env;
 
 #[no_mangle]
@@ -20,7 +20,6 @@ pub async fn run() -> anyhow::Result<()> {
     let login = env::var("login").unwrap_or("juntao".to_string());
     let owner = env::var("owner").unwrap_or("juntao".to_string());
     let repo = env::var("repo").unwrap_or("test".to_string());
-    let openai_key_name = env::var("openai_key_name").unwrap_or("global.free.trial".to_string());
     let trigger_phrase = env::var("trigger_phrase").unwrap_or("flows summarize".to_string());
 
     let events = vec!["pull_request", "issue_comment"];
@@ -29,7 +28,6 @@ pub async fn run() -> anyhow::Result<()> {
             &login,
             &owner,
             &repo,
-            &openai_key_name,
             &trigger_phrase,
             payload,
         )
@@ -43,7 +41,6 @@ async fn handler(
     login: &str,
     owner: &str,
     repo: &str,
-    openai_key_name: &str,
     trigger_phrase: &str,
     payload: EventPayload,
 ) {
@@ -150,7 +147,7 @@ async fn handler(
             retry_times: 3,
         };
         let question = "The following is a GitHub patch. Please summarize the key changes and identify potential problems. Start with the most important findings.\n\n".to_string() + commit;
-        if let Some(r) = chat_completion(openai_key_name, &chat_id, &question, &co) {
+        if let Some(r) = chat_completion_default_key(&chat_id, &question, &co) {
             write_error_log!("Got a patch summary");
             if reviews_text.len() < 9000 {
                 reviews_text.push_str("------\n");
@@ -173,7 +170,7 @@ async fn handler(
             retry_times: 3,
         };
         let question = "Here is a set of summaries for software source code patches. Each summary starts with a ------ line. Please write an overall summary considering all the individual summary. Please present the potential issues and errors first, following by the most important findings, in your summary.\n\n".to_string() + &reviews_text;
-        if let Some(r) = chat_completion(openai_key_name, &chat_id, &question, &co) {
+        if let Some(r) = chat_completion_default_key(&chat_id, &question, &co) {
             write_error_log!("Got the overall summary");
             resp.push_str(&r.choice);
             resp.push_str("\n\n## Details\n\n");
